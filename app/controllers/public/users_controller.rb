d046fd377762a5d4f28efb115999d0fbc467e1e7
TakedaKaho <tktk.a3104c@gmail.com>
2024-07-03 04:02:34 +0000

    mypage途中

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,11 +1,19 @@
 class Public::UsersController < ApplicationController
     def show
+     @user = User.find(params[:id])
     end 
     
     def edit
+     @user = User.find(params[:id])
     end
     
     def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+       redirect_to @user, notice: "Profile image updated successfully."
+      else
+       render :edit
+      end
     end 
     
     def check
@@ -13,5 +21,11 @@ class Public::UsersController < ApplicationController
     
     def out
     end 
+    
+  private
+
+    def user_params
+     params.require(:user).permit(:profile_image)
+    end
     
 end
