8fb23aa381bbde5e198723db3001e771bba7bb32
TakedaKaho <tktk.a3104c@gmail.com>
2024-07-24 08:57:47 +0000

    admin/tagフラッシュメセージ削除ver追加

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -28,7 +28,7 @@ class Admin::TagsController < ApplicationController
     def update
      @tag =Tag.find(params[:id])
      if @tag.update(tag_params)
-       flash[:tag_update_notice]="Tag名更新しました"
+       flash[:tag_update_notice]="Tag名更新しました。"
        redirect_to admin_tags_path
      else
        render edit
@@ -38,6 +38,7 @@ class Admin::TagsController < ApplicationController
     def destroy
       @tag =Tag.find(params[:id])
       @tag.destroy
+      flash[:tag_destroy_notice]="Tagを正常に削除しました。"
       redirect_to admin_tags_path
     end 
     
