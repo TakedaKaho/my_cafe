e08652cb58a60dfb78a5a29a1e2c291b72affc40
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-14 08:35:01 +0000

    adminダッシュボード

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,8 @@ class Admin::HomesController < ApplicationController
     before_action :authenticate_admin!
     
     def top
+     @total_users = User.count
+     @total_comments = Comment.count
+     @recent_users = User.order(created_at: :desc).limit(5)
     end 
 end
