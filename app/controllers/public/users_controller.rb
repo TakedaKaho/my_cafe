e230fa2e93a933f9d7bc9bafb16d463441258932
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-21 04:11:00 +0000

    edit権限修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,40 +1,48 @@
 class Public::UsersController < ApplicationController
- before_action :authenticate_user!
- 
-    def show
-     @user = User.find(params[:id])
-     @liked_posts = @user.likes.map(&:post)
-    end 
-    
-    def edit
-     @user = User.find(params[:id])
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update, :authorize_user]
+  before_action :authorize_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @liked_posts = @user.likes.map(&:post)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:edit_notice] = "プロフィールが正常に変更されました"
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
-    
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-       flash[:edit_notice] = "プロフィールが正常に変更されました"
-       redirect_to user_path(@user)
-      else
-       render :edit
-      end
-    end 
-    
-    def check
-    end 
-    
-    def out
-     @user = User.find(current_user.id)
-     #デフォルトはfalse
-     @user.update(is_deleted: true)
-     reset_session
-     redirect_to root_path
-    end
-    
+  end
+
+  def check
+  end
+
+  def out
+    @user = User.find(current_user.id)
+    # デフォルトは false
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
   private
 
-    def user_params
-     params.require(:user).permit(:profile_image, :name, :body, :email)
-    end
-    
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_to root_path unless @user == current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :body, :email)
+  end
 end
+
